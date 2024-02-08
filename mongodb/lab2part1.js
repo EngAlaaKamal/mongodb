ef0a326745c74eee57601b848cd666949601d188
EngAlaaKamal <147073553+EngAlaaKamal@users.noreply.github.com>
2024-02-08 11:06:04 +0200

    Update lab2part1.js

--- mongodb/lab2part1.js
+++ mongodb/lab2part1.js
@@ -1,155 +1,212 @@
- 
- use inventoryDB
-  //find count products in each category
-db.products.aggregate([
-  
-  { $group : {_id: '$category' , count:  {$sum:1} } } ,
-  { $match : {_id: {$ne:null} } } ,
- 
-])
-  
- 
-//Display max category products price.  
-  
-db.products.aggregate([
-  
-  { $group : {_id: '$category' , maxPrice: {$max:"$price"} } } ,
-  { $match : {_id: {$ne:null} } } ,
-  
-])
-
-
- 
-// display user ahmed orders populated with product
-  
-db.users.aggregate([
-{
-    $match: {name: "ahmed"}
-},
-{
-    $lookup: {
-        from: "orders",
-        localField: "_id",
-        foreignField: "userId",
-        as: "userorders" }
-},
-{
-    $lookup: {
-        from: "products",
-        localField: "orders.productsIds",
-        foreignField: "_id",
-        as: "userproducts"
-    }
-},
-    
-])
-//max price
-db.users.aggregate([
-    {
-        $match: {
-            name: "ahmed"
-        }
-    },
-    {
-        $lookup: {
-            from: "orders",
-            localField: "_id",
-            foreignField: "userId",
-            as: "userorders"
-        }
-    },
-    {
-        $unwind: "$userorders"
-    },
-    {
-        $lookup: {
-            from: "products",
-            localField: "userorders.productsIds",
-            foreignField: "_id",
-            as: "userproducts"
-        }
-    },
-    {
-        $unwind: "$userproducts"
-    },
-    {
-        $group: {
-            _id: {
-                userId: "$_id",
-                orderId: "$userorders._id"
-            },
-            maxtotalPrice: {
-                $sum: {
-                    $multiply: ["$userproducts.price", "$userproducts.quantity"]
-                }
-            }
-        }
-    },
-    {
-     $sort: {
-      maxtotalPrice: -1
-    }
-  },
-   
-  {
-    $limit: 1
-  }
-])
-    
- //-----------------------------------------------------------
-  // some operation on book collection 
- db.books.aggregate([
-{
-    $match: {
-        pageCount: {$gt: 0},
-        publishedDate: {
-            $gte: ISODate("2008-01-01T00:00:00.000Z"),
-             
-        }
-    }
-},
-{
-     $sort: {pageCount: -1}
-},
-{
-    $unwind: "$authors"
-},
- 
-{
-    $match: { authors: "Danno Ferrin"}
-},{
-    $group: {
-      _id: "$authors",
-      bookCount: { $sum: 1 },
-      totalPageCount: { $sum: "$pageCount" }
-    }
-  }
-]);
-
-])
-
-//=============== Q.2 ====================
-db.books.aggregate([
-{
-    $unwind: "$authors"
-},
-{
-    $group: {
-        _id: "$authors",
-        name: {
-            $first: "$authors"
-        },
-        booksPublishedCount: {$count: {}}
-    }
-},
-{
-    $project: { _id: false}
-}
-])
-  
-  
-  
-  
-  
-  
-  
+ 
+
+ use inventoryDB
+
+  //find count products in each category
+
+db.products.aggregate([
+
+  
+
+  { $group : {_id: '$category' , count:  {$sum:1} } } ,
+
+  { $match : {_id: {$ne:null} } } ,
+
+ 
+
+])
+
+  
+
+ 
+
+//Display max category products price.  
+
+  
+
+db.products.aggregate([
+
+  
+
+  { $group : {_id: '$category' , maxPrice: {$max:"$price"} } } ,
+
+  { $match : {_id: {$ne:null} } } ,
+
+  
+
+])
+
+
+
+
+
+ 
+
+// display user ahmed orders populated with product
+
+  
+
+db.users.aggregate([
+
+{
+
+    $match: {name: "ahmed"}
+
+},
+
+{
+
+    $lookup: {
+
+        from: "orders",
+
+        localField: "_id",
+
+        foreignField: "userId",
+
+        as: "userorders" }
+
+},
+
+{
+
+    $lookup: {
+
+        from: "products",
+
+        localField: "orders.productsIds",
+
+        foreignField: "_id",
+
+        as: "userproducts"
+
+    }
+
+},
+
+    
+
+])
+
+//max price
+
+db.users.aggregate([
+
+    {
+
+        $match: {
+
+            name: "ahmed"
+
+        }
+
+    },
+
+    {
+
+        $lookup: {
+
+            from: "orders",
+
+            localField: "_id",
+
+            foreignField: "userId",
+
+            as: "userorders"
+
+        }
+
+    },
+
+    {
+
+        $unwind: "$userorders"
+
+    },
+
+    {
+
+        $lookup: {
+
+            from: "products",
+
+            localField: "userorders.productsIds",
+
+            foreignField: "_id",
+
+            as: "userproducts"
+
+        }
+
+    },
+
+    {
+
+        $unwind: "$userproducts"
+
+    },
+
+    {
+
+        $group: {
+
+            _id: {
+
+                userId: "$_id",
+
+                orderId: "$userorders._id"
+
+            },
+
+            maxtotalPrice: {
+
+                $sum: {
+
+                    $multiply: ["$userproducts.price", "$userproducts.quantity"]
+
+                }
+
+            }
+
+        }
+
+    },
+
+    {
+
+     $sort: {
+
+      maxtotalPrice: -1
+
+    }
+
+  },
+
+   
+
+  {
+
+    $limit: 1
+
+  }
+
+])
+
+    
+
+ 
+
+  
+
+  
+
+  
+
+  
+
+  
+
+  
+
+  
+
